ceeb4584938979bef8995f3b02ce10c06fd8a25e
malaika <malaikachauhan5@gmail.com>
2024-09-20 18:45:55 +0500

    hello

---
+++ my/script.js
@@ -0,0 +1,17 @@
+var arr=["ali","ahmad","akram"];
+var arr2=["aiman","amjad","aslam"];
+var arr3=["aliyan","a","alee"];
+
+for(var i=0; i<=arr.length; i++)
+    {
+        for(var j=0; j<=arr2.length;j++)
+        {
+            for(var k=0; k<=arr3.length;j++)   
+            if (i==j==j)
+                {
+                 console.log(arr[i] + " " + arr2[j]+ " " + arr3);
+         
+                }
+        }
+        
+    }
